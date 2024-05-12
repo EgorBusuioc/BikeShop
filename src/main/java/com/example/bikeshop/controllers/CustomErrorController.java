9c6be70c4ace453c5dffd1fb9e4d161470731a9e
Egor Busuioc <busuiocegor@gmail.com>
2024-05-12 17:59:07 +0300

    1) Added final design page for type bike 2) Changed database credentials 3) Added error page

--- src/main/java/com/example/bikeshop/controllers/CustomErrorController.java
+++ src/main/java/com/example/bikeshop/controllers/CustomErrorController.java
@@ -1,34 +1,38 @@
-//package com.example.bikeshop.controllers;
-//
-//import jakarta.servlet.RequestDispatcher;
-//import jakarta.servlet.http.HttpServletRequest;
-//import org.springframework.boot.web.servlet.error.ErrorController;
-//import org.springframework.http.HttpStatus;
-//import org.springframework.stereotype.Controller;
-//import org.springframework.web.bind.annotation.RequestMapping;
-//
-///**
-// * @author EgorBusuioc
-// * 08.05.2024
-// */
-//
-//@Controller
-//public class CustomErrorController implements ErrorController {
-//
-//    @RequestMapping("/error")
-//    public String handleError(HttpServletRequest request) {
-//        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-//
-//        if (status != null) {
-//            Integer statusCode = Integer.valueOf(status.toString());
-//
-//            if(statusCode ==  HttpStatus.NOT_FOUND.value()) {
-//                return "error";
-//            }
-//            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-//                return "error";
-//            }
-//        }
-//        return "error";
-//    }
-//}
+package com.example.bikeshop.controllers;
+
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import org.springframework.boot.web.servlet.error.ErrorController;
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * @author EgorBusuioc
+ * 08.05.2024
+ */
+
+@Controller
+public class CustomErrorController implements ErrorController {
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request, Model model) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode ==  HttpStatus.NOT_FOUND.value()) {
+                model.addAttribute("error", "404, page not found.");
+                return "error";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                model.addAttribute("error", "500, internal server error.");
+                return "error";
+            }
+        }
+
+        return "error";
+    }
+}
